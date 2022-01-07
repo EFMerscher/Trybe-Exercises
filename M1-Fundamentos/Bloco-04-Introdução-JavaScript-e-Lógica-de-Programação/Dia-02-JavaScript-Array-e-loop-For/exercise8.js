e302902a796fb1a667f7ae0e32ad67fe7fa16bed
Ed <efmerscher@gmail.com>
2022-01-07 12:32:00 -0300

    exercise8-dia02-bloco-04

---
+++ M1-Fundamentos/Bloco-04-Introdução-JavaScript-e-Lógica-de-Programação/Dia-02-JavaScript-Array-e-loop-For/exercise8.js
@@ -0,0 +1,9 @@
+// 8. Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let array = [];
+for (let number = 1; number <= 25; number += 1) {
+  array.push(number)
+}
+console.log(array);
