aafcd6abfbb7369776528d1a7d282f225b357608
Ed <efmerscher@gmail.com>
2022-07-04 17:54:24 -0300

    exercicios dia bloco 10 dia 01

---
+++ M2-Desenvolvimento-Front-End/Bloco-10-Introdução-à-React/Dia-01-Hello-World-no-React/first-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+
+const textJSX = <h1>Hello JSX</h1>;
+
+class App extends React.Component {
+  render () {
+    return (
+      textJSX
+    )
+  }
+}
+
+export default App;
