b86952f0ddd58219e6f3cf8cab4ba45844435934
Ed <efmerscher@gmail.com>
2022-07-05 23:42:37 -0300

    exercicio 7 de fixacao

--- M2-Desenvolvimento-Front-End/Bloco-11-Componentes-com-Estado-Eventos-e-Formulários-com-React/Dia-01-Componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ M2-Desenvolvimento-Front-End/Bloco-11-Componentes-com-Estado-Eventos-e-Formulários-com-React/Dia-01-Componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -16,29 +16,32 @@ class App extends React.Component {
   }
 
   handleButtonOne() {
-    this.setState(() => ({
-      clicksBtnOne: 1,
+    this.setState((prevState) => ({
+      clicksBtnOne: prevState.clicksBtnOne + 1,
     }));
   }
   
   handleButtonTwo() {
-    this.setState(() => ({
-      clicksBtnTwo: 1,
+    this.setState((prevState) => ({
+      clicksBtnTwo: prevState.clicksBtnTwo + 1,
     }));
   }
   
   handleButtonThree() {
-    this.setState(() => ({
-      clicksBtnThree: 1,
+    // A única coisa diferente é a desconstrução.
+    // Apenas um método diferente de fazer a mesma coisa
+    // do que foi feito nas funções anteriores.
+    this.setState(({ clicksBtnThree }) => ({
+      clicksBtnThree: clicksBtnThree + 1,
     }));
   }
- 
-   render() {
+  
+  render() {
     return (
       <div>
-        <button onClick={ this.handleButtonOne }>Botão 1</button>
-        <button onClick={ this.handleButtonTwo }>Botão 2</button>
-        <button onClick={ this.handleButtonThree }>Botão 3</button>
+        <button onClick={ this.handleButtonOne }>Botão 1 | Count = {this.state.clicksBtnOne}</button>
+        <button onClick={ this.handleButtonTwo }>Botão 2 | Count = {this.state.clicksBtnTwo}</button>
+        <button onClick={ this.handleButtonThree }>Botão 3 | Count = {this.state.clicksBtnThree}</button>
       </div>
     );
   }
